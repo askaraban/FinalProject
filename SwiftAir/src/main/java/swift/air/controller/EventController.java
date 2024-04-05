94b5e81f3fd5270bc8cb1e989e1df3d3f93a8b96
itwill <itwill@DESKTOP-DOGASU0>
2024-04-05 19:08:03 +0900

    EventController 수정중, util에서 filedownload 삭제

--- SwiftAir/src/main/java/swift/air/controller/EventController.java
+++ SwiftAir/src/main/java/swift/air/controller/EventController.java
@@ -2,13 +2,21 @@ package swift.air.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.context.WebApplicationContext;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
+import swift.air.service.EventService;
 
 @Controller
 @RequestMapping("/event")
+@RequiredArgsConstructor
 public class EventController {
-	@RequestMapping(value = "/event/add")
+	private final WebApplicationContext context;
+	private final EventService eventService;
+	
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
 	public String eventAdd() {
 		return "event/event_add";
 	}
