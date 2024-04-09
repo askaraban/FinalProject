f740fc905ba8237242bde92625682fd01c5bad33
itwill <itwill@DESKTOP-E18OM1D>
2024-04-09 19:01:33 +0900

    faq 메퍼 추가

--- SwiftAir/src/main/java/swift/air/mapper/FaqMapper.java
+++ SwiftAir/src/main/java/swift/air/mapper/FaqMapper.java
@@ -1,5 +1,14 @@
 package swift.air.mapper;
 
-public interface FaqMapper {
+import java.util.List;
+import java.util.Map;
 
+import swift.air.dto.Faq;
+
+public interface FaqMapper {
+	int insertFaq(Faq faq);
+	int updateFaq(Faq faq);
+	int deleteFaq(int faqId);
+	int selectFaqCount();
+	List<Map<String, Object>> selectFaqList(Map<String, Object> map);
 }
