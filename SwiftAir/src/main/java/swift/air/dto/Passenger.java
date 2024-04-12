8ef3943f229c0eca927770ab5b58f094d80a91c4
itwill <itwill@DESKTOP-Q8K2VFI>
2024-04-12 11:46:07 +0900

    passengerDTO 수정

--- SwiftAir/src/main/java/swift/air/dto/Passenger.java
+++ SwiftAir/src/main/java/swift/air/dto/Passenger.java
@@ -3,16 +3,14 @@ package swift.air.dto;
 import lombok.Data;
 
 /*
-이름                     널? 유형           
----------------------- -- ------------ 
-PASSENGER_LAST_NAME       VARCHAR2(20) 
-PASSENGER_FIRST_NAME      VARCHAR2(20) 
-PASSENGER_BIRTH           DATE         
-PASSENGER_PAYMENT_ID      VARCHAR2(20) 
-PASSENGER_SEAT_NAME1      VARCHAR2(10) 
-PASSENGER_SCHEDULE_ID1    VARCHAR2(20) 
-PASSENGER_SEAT_NAME2      VARCHAR2(10) 
-PASSENGER_SCHEDULE_ID2    VARCHAR2(10)
+이름                    널? 유형           
+--------------------- -- ------------ 
+PASSENGER_LAST_NAME      VARCHAR2(20) 
+PASSENGER_FIRST_NAME     VARCHAR2(20) 
+PASSENGER_BIRTH          DATE         
+PASSENGER_PAYMENT_ID     VARCHAR2(20) 
+PASSENGER_SEAT_NAME      VARCHAR2(10) 
+PASSENGER_SCHEDULE_ID    VARCHAR2(20)
 */
 
 @Data
@@ -21,8 +19,6 @@ public class Passenger {
 	private String passengerFirstName; 
 	private String passengerBirth; 
 	private String passengerPaymentId; 
-	private String passengerSeatName1;
-	private String passengerScheduleId1;
-	private String passengerSeatName2;
-	private String passengerScheduleId2;
+	private String passengerSeatName;
+	private String passengerScheduleId;
 }
